c80aaf4c1efce9236b8c9c500bab7c660cdd28f5
zq2599 <zq2599@gmail.com>
2018-01-21 11:32:39 +0800

    修改返回内容

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        return "abcdef. Hello jenkins, " + new Date();
+        return "abcdefg. Hello jenkins, " + new Date();
     }
 }
